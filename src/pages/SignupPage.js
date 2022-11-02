ace74dc92b37dd35313417244dd78c7a2bd9bf15
Kay Bi <kaybi9x@gmail.com>
2022-11-02 18:22:29 +0700

    update login GG

--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -7,82 +7,84 @@ import useResponsive from '../hooks/useResponsive';
 // components
 import Logo from '../components/logo';
 // sections
-import SignupForm from "../sections/auth/login/SignupForm";
+import SignupForm from '../sections/auth/login/SignupForm';
 
 // ----------------------------------------------------------------------
 
 const StyledRoot = styled('div')(({ theme }) => ({
-    [theme.breakpoints.up('md')]: {
-        display: 'flex',
-    },
+  [theme.breakpoints.up('md')]: {
+    display: 'flex',
+  },
 }));
 
 const StyledSection = styled('div')(({ theme }) => ({
-    width: '100%',
-    maxWidth: 480,
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    boxShadow: theme.customShadows.card,
-    backgroundColor: theme.palette.background.default,
+  width: '100%',
+  maxWidth: 480,
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  boxShadow: theme.customShadows.card,
+  backgroundColor: theme.palette.background.default,
 }));
 
 const StyledContent = styled('div')(({ theme }) => ({
-    maxWidth: 480,
-    margin: 'auto',
-    minHeight: '100vh',
-    display: 'flex',
-    justifyContent: 'center',
-    flexDirection: 'column',
-    padding: theme.spacing(12, 0),
+  maxWidth: 480,
+  margin: 'auto',
+  minHeight: '100vh',
+  display: 'flex',
+  justifyContent: 'center',
+  flexDirection: 'column',
+  padding: theme.spacing(12, 0),
 }));
 
 // ----------------------------------------------------------------------
 
 function SignupPage() {
-    const mdUp = useResponsive('up', 'md');
+  const mdUp = useResponsive('up', 'md');
 
-    return (
-        <>
-            <Helmet>
-                <title> Sign Up </title>
-            </Helmet>
+  return (
+    <>
+      <Helmet>
+        <title> Sign Up | Money Manager Master </title>
+      </Helmet>
 
-            <StyledRoot>
-                <Logo
-                    sx={{
-                        position: 'fixed',
-                        top: { xs: 16, sm: 24, md: 40 },
-                        left: { xs: 16, sm: 24, md: 40 },
-                    }}
-                />
+      <StyledRoot>
+        <Logo
+          sx={{
+            position: 'fixed',
+            top: { xs: 16, sm: 24, md: 40 },
+            left: { xs: 16, sm: 24, md: 40 },
+          }}
+        />
 
-                {mdUp && (
-                    <StyledSection>
-                        <Typography variant="h3" sx={{ px: 5, mt: 10, mb: 5 }}>
-                            Hi, Welcome to  our World!
-                        </Typography>
-                        <img src="/assets/illustrations/illustration_login.png" alt="login" />
-                    </StyledSection>
-                )}
+        {mdUp && (
+          <StyledSection>
+            <Typography variant="h3" sx={{ px: 5, mt: 10, mb: 5 }}>
+              Hi, Welcome to our World!
+            </Typography>
+            <img src="/assets/illustrations/illustration_login.png" alt="login" />
+          </StyledSection>
+        )}
 
-                <Container maxWidth="sm">
-                    <StyledContent>
-                        <Typography variant="h4" gutterBottom>
-                            Sign up to 395
-                        </Typography>
+        <Container maxWidth="sm">
+          <StyledContent>
+            <Typography variant="h4" gutterBottom>
+              Sign up to 395
+            </Typography>
 
-                        <Typography variant="body2" sx={{ mb: 5 }}>
-                            Already have an account? {''}
-                            <Link href="/login" variant="subtitle2">Sign in</Link>
-                        </Typography>
+            <Typography variant="body2" sx={{ mb: 5 }}>
+              Already have an account? {''}
+              <Link href="/login" variant="subtitle2">
+                Sign in
+              </Link>
+            </Typography>
 
-                        <SignupForm />
-                    </StyledContent>
-                </Container>
-            </StyledRoot>
-        </>
-    );
+            <SignupForm />
+          </StyledContent>
+        </Container>
+      </StyledRoot>
+    </>
+  );
 }
 
 export default SignupPage;
--- src/pages/WalletPage.js
+++ src/pages/WalletPage.js
@@ -1,144 +1,147 @@
 import { Helmet } from 'react-helmet-async';
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import {
-    Avatar,
-    Box,
-    Button,
-    Dialog, DialogActions, DialogContent, DialogContentText,
-    DialogTitle,
-    Grid,
-    List,
-    ListItem,
-    ListItemAvatar,
-    ListItemText,
-    Paper, TextField
-} from "@mui/material";
-import wallet from '../_mock/wallet'
+  Avatar,
+  Box,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Grid,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Paper,
+  TextField,
+} from '@mui/material';
+import wallet from '../_mock/wallet';
 
 function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
 
 export default function WalletPage() {
-    const [detail,setDetail] = useState(<h5>Choose wallet to see details</h5>)
-    const [wallets,setWallets] = useState(wallet)
-    const [walletEdit,setWalletEdit] = useState([])
-    const [open, setOpen] = useState(false);
+  const [detail, setDetail] = useState(<h5>Choose wallet to see details</h5>);
+  const [wallets, setWallets] = useState(wallet);
+  const [walletEdit, setWalletEdit] = useState([]);
+  const [open, setOpen] = useState(false);
 
-    const handleClickOpen = (id) => {
-        const walletEdit = wallets.filter(wallet => wallet._id === id)
-        setWalletEdit(walletEdit[0]);
-        setOpen(true);
-    };
+  const handleClickOpen = (id) => {
+    const walletEdit = wallets.filter((wallet) => wallet._id === id);
+    setWalletEdit(walletEdit[0]);
+    setOpen(true);
+  };
 
-    const handleClose = () => {
-        setOpen(false);
-    };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-    useEffect(() => {
+  const onChangeEdit = (e) => {
+    setWalletEdit({ ...walletEdit, [e.target.name]: e.target.value });
+  };
 
-    },wallets)
+  const handleSaveEdit = (id) => {
+    const index = wallets.findIndex((wallet) => wallet._id === id);
+    wallets[index] = walletEdit;
+    setWallets(wallets);
+    setOpen(false);
+  };
 
-    const onChangeEdit = (e) => {
-        setWalletEdit({...walletEdit, [e.target.name]: e.target.value});
-    }
+  const handleClick = (id) => {
+    const wallet = wallets.filter((wallet) => wallet._id === id);
+    setDetail(
+      <>
+        <h4>Wallet detail</h4>
+        <hr />
+        <p>Wallet Name: {wallet[0].name}</p>
+        <hr />
+        <p>Wallet Amount: {numberWithCommas(wallet[0].amount)}</p>
+        <hr />
+        <Button variant="contained" color="primary" onClick={() => handleClickOpen(id)}>
+          Edit
+        </Button>
+        <Button variant="contained" color="error">
+          Delete
+        </Button>
+      </>
+    );
+  };
 
-    const handleSaveEdit = (id) => {
-        const index = wallets.findIndex(wallet => wallet._id === id);
-        wallets[index] = walletEdit
-        setWallets(wallets)
-        setOpen(false);
-    }
+  return (
+    <>
+      <Helmet>
+        <title> Wallet | Money Manager Master </title>
+      </Helmet>
 
-    const handleClick = (id) => {
-        const wallet = wallets.filter(wallet => wallet._id === id)
-        setDetail(
-            <>
-                <h4>Wallet detail</h4>
-                <hr/>
-                <p>Wallet Name: {wallet[0].name}</p>
-                <hr/>
-                <p>Wallet Amount: {numberWithCommas(wallet[0].amount)}</p>
-                <hr/>
-                <Button variant="contained" color="primary" onClick={()=>handleClickOpen(id)}>Edit</Button>
-                <Button variant="contained" color="error">Delete</Button>
-            </>
-        )
-    }
+      <h1>Wallet</h1>
+      <Paper elevation={3} sx={{ padding: 2 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={5} alignItems="center">
+            <div>Total: 10.000.000</div>
+          </Grid>
+          <Grid item xs={4}>
+            <h3>Detail</h3>
+          </Grid>
+          <Grid item xs={3}>
+            <Box display="flex" justifyContent="flex-end">
+              <Button variant="contained" color="success">
+                Add new Wallet
+              </Button>
+            </Box>
+          </Grid>
+          <Grid item xs={5}>
+            <List>
+              {wallets.slice(0, 3).map((item) => (
+                <ListItem button onClick={() => handleClick(item._id)} key={item._id}>
+                  <ListItemAvatar>
+                    <Avatar alt={item.name} src={item.icon} />
+                  </ListItemAvatar>
+                  <ListItemText primary={item.name} secondary={numberWithCommas(item.amount)} />
+                </ListItem>
+              ))}
+            </List>
+          </Grid>
+          <Grid item xs={7}>
+            <div>{detail}</div>
+          </Grid>
+        </Grid>
+      </Paper>
 
-    return(
-        <>
-            <Helmet>
-                <title> Wallet | Money Controller </title>
-            </Helmet>
-
-            <h1>Wallet</h1>
-            <Paper elevation={3} sx={{padding:2}}>
-                <Grid container spacing={2}>
-                    <Grid item xs={5} alignItems="center" >
-                        <div>Total: 10.000.000</div>
-                    </Grid>
-                    <Grid item xs={4}>
-                        <h3>Detail</h3>
-                    </Grid>
-                    <Grid item xs={3}>
-                        <Box display="flex" justifyContent="flex-end">
-                            <Button variant="contained" color="success">Add new Wallet</Button>
-                        </Box>
-                    </Grid>
-                    <Grid item xs={5}>
-                        <List>
-                            {wallets.map((item) =>(
-                            <ListItem button onClick={()=>handleClick(item._id)} key={item._id}>
-                                <ListItemAvatar>
-                                    <Avatar alt={item.name} src={item.icon} />
-                                </ListItemAvatar>
-                                <ListItemText primary={item.name} secondary={numberWithCommas(item.amount)} />
-                            </ListItem>
-                            ))
-                            }
-                        </List>
-                    </Grid>
-                    <Grid item xs={7}>
-                        <div>{detail}</div>
-                    </Grid>
-                </Grid>
-            </Paper>
-
-            <Dialog open={open} onClose={handleClose}>
-                <DialogTitle>Edit wallet</DialogTitle>
-                <DialogContent>
-                    <DialogContentText>
-                        Please enter your update infomation
-                    </DialogContentText>
-                    <TextField
-                        autoFocus
-                        margin="dense"
-                        name="name"
-                        label="Wallet Name"
-                        type="text"
-                        fullWidth
-                        variant="standard"
-                        value={walletEdit.name}
-                        onChange={onChangeEdit}
-                    />
-                    <TextField
-                        autoFocus
-                        margin="dense"
-                        name="amount"
-                        label="Wallet Amount"
-                        type="number"
-                        fullWidth
-                        variant="standard"
-                        value={walletEdit.amount}
-                        onChange={onChangeEdit}
-                    />
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose}>Cancel</Button>
-                    <Button onClick={()=>handleSaveEdit(walletEdit._id)}>Save</Button>
-                </DialogActions>
-            </Dialog>
-        </>
-    )
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Edit wallet</DialogTitle>
+        <DialogContent>
+          <DialogContentText>Please enter your update infomation</DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            name="name"
+            label="Wallet Name"
+            type="text"
+            fullWidth
+            variant="standard"
+            value={walletEdit.name}
+            onChange={onChangeEdit}
+          />
+          <TextField
+            autoFocus
+            margin="dense"
+            name="amount"
+            label="Wallet Amount"
+            type="number"
+            fullWidth
+            variant="standard"
+            value={walletEdit.amount}
+            onChange={onChangeEdit}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={() => handleSaveEdit(walletEdit._id)}>Save</Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
 }
