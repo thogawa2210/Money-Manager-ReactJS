26c11d976bb6a113b983c71d98879b47b892f1e0
Kay Bi <kaybi9x@gmail.com>
2022-11-03 22:43:28 +0700

    fix bug

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -3,12 +3,21 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 // @mui
 import * as React from 'react';
-import { Card, Container, Button } from '@mui/material';
+import {
+  Card,
+  Container,
+  Button,
+  TextField,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from '@mui/material';
 import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
 import Avatar from '@mui/material/Avatar';
 import { red } from '@mui/material/colors';
 
@@ -19,6 +28,25 @@ import { red } from '@mui/material/colors';
 // ----------------------------------------------------------------------
 
 export default function UserPage() {
+  const [openName, setOpenName] = useState(false);
+  const [openPass, setOpenPass] = useState(false);
+
+  const handleClickOpenName = () => {
+    setOpenName(true);
+  };
+
+  const handleCloseName = () => {
+    setOpenName(false);
+  };
+
+  const handleClickOpenPass = () => {
+    setOpenPass(true);
+  };
+
+  const handleClosePass = () => {
+    setOpenPass(false);
+  };
+
   const [form, setForm] = useState({
     username: '',
     email: '',
@@ -51,24 +79,76 @@ export default function UserPage() {
       </Helmet>
 
       <Container>
-        <Card sx={{ boxShadow: '1px 1px 1px 1px', mt: 3, maxWidth: 700 }}>
+        <Card sx={{ boxShadow: '1px 1px 1px 1px #CCE2FF', mt: 2, maxWidth: 500, maxHeight: 500, ml: 20 }}>
           <CardHeader
-            avatar={<Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-              
-            </Avatar>}
+            avatar={<Avatar sx={{ bgcolor: red[500] }} aria-label="recipe"></Avatar>}
             title={`${form.username}`}
             subheader={`${form.email}`}
           />
-          <CardMedia component="img" height="194" image="" alt="user photo" />
+          <CardMedia component="img" height="194" image="/assets/images/avatars/avatar_10.jpg" alt="user photo" />
           <CardContent>
-            <Button fullWidth variant="contained" color="info" size='large'>
-              Edit
+            <Button fullWidth variant="contained" color="info" size="large" onClick={handleClickOpenName}>
+              Edit Username
+            </Button>
+            <Button
+              fullWidth
+              variant="outlined"
+              color="success"
+              size="large"
+              sx={{ mt: 1 }}
+              onClick={handleClickOpenPass}
+            >
+              Change Password
             </Button>
           </CardContent>
           <CardActions disableSpacing></CardActions>
-          <Collapse timeout="auto" unmountOnExit></Collapse>
         </Card>
       </Container>
+
+      <Dialog open={openName} onClose={handleCloseName}>
+        <DialogTitle>Subscribe</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            To subscribe to this website, please enter your email address here. We will send updates occasionally.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="New Username"
+            type="text"
+            fullWidth
+            variant="standard"
+            required
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseName}>Cancel</Button>
+          <Button onClick={handleCloseName}>Subscribe</Button>
+        </DialogActions>
+      </Dialog>
+
+      <Dialog open={openPass} onClose={handleClosePass}>
+        <DialogTitle>Subscribe</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            To subscribe to this website, please enter your email address here. We will send updates occasionally.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Email Address"
+            type="email"
+            fullWidth
+            variant="standard"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClosePass}>Cancel</Button>
+          <Button onClick={handleClosePass}>Subscribe</Button>
+        </DialogActions>
+      </Dialog>
     </>
   );
 }
