0fd54b17c8ad772ba5574644bd44bbc5b2c0e488
Kay Bi <kaybi9x@gmail.com>
2022-11-03 18:41:56 +0700

    update user page

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -1,173 +1,74 @@
 import { Helmet } from 'react-helmet-async';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 // @mui
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import {Card, Container, Stack, Popover,MenuItem} from '@mui/material';
+import { Card, Container, Button } from '@mui/material';
 import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
 import Collapse from '@mui/material/Collapse';
 import Avatar from '@mui/material/Avatar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
 import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+
 // components
-import Iconify from '../components/iconify';
-
 
 // ----------------------------------------------------------------------
 
-
 // ----------------------------------------------------------------------
-const ExpandMore = styled((props) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-  marginLeft: 'auto',
-  transition: theme.transitions.create('transform', {
-    duration: theme.transitions.duration.shortest,
-  }),
-}));
 
 export default function UserPage() {
-  const [open, setOpen] = useState(null);
-  const [expanded, setExpanded] = React.useState(false);
+  const [form, setForm] = useState({
+    username: '',
+    email: '',
+    password: '',
+    google_id: '',
+  });
 
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
+  const userApi = async (user_id) => {
+    return await axios.get(`http://localhost:3001/user/info/${user_id}`);
   };
 
-  const [form, setForm] = useState();
-
-
-  const handleCloseMenu = () => {
-    setOpen(null);
-  };
+  useEffect(() => {
+    const user_id = JSON.parse(localStorage.getItem('user')).user_id;
+    userApi(user_id)
+      .then((res) => {
+        if (res.data.type === 'success') {
+          let userInfo = res.data.message;
+          setForm(userInfo);
+        } else {
+          console.log(res.data.message);
+        }
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
     <>
       <Helmet>
-        <title> User | Money Manager Master </title>
+        <title> Profile | Money Manager Master </title>
       </Helmet>
 
-      <Container sx={{ boxShadow: '2px 2px 2px 2px', ml: 2, width: '90%', mt: 10, padding: 2 }}>
-        <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
-          <Typography variant="h4" gutterBottom>
-            Profile
-          </Typography>
-
-          <Card sx={{ maxWidth: 345 }}>
-      <CardHeader
-        avatar={
-          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-            R
-          </Avatar>
-        }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title="Shrimp and Chorizo Paella"
-        subheader="September 14, 2016"
-      />
-      <CardMedia
-        component="img"
-        height="194"
-        image="/static/images/cards/paella.jpg"
-        alt="Paella dish"
-      />
-      <CardContent>
-        <Typography variant="body2" color="text.secondary">
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the mussels,
-          if you like.
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
-        <ExpandMore
-          expand={expanded}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
-        </ExpandMore>
-      </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <Typography paragraph>Method:</Typography>
-          <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-            aside for 10 minutes.
-          </Typography>
-          <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
-            medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
-            occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
-            large plate and set aside, leaving chicken and chorizo in the pan. Add
-            pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
-            stirring often until thickened and fragrant, about 10 minutes. Add
-            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-          </Typography>
-          <Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and
-            peppers, and cook without stirring, until most of the liquid is absorbed,
-            15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
-            mussels, tucking them down into the rice, and cook again without
-            stirring, until mussels have opened and rice is just tender, 5 to 7
-            minutes more. (Discard any mussels that don&apos;t open.)
-          </Typography>
-          <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then serve.
-          </Typography>
-        </CardContent>
-      </Collapse>
-    </Card>
-        </Stack>
+      <Container>
+        <Card sx={{ boxShadow: '1px 1px 1px 1px', mt: 3, maxWidth: 700 }}>
+          <CardHeader
+            avatar={<Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+              
+            </Avatar>}
+            title={`${form.username}`}
+            subheader={`${form.email}`}
+          />
+          <CardMedia component="img" height="194" image="" alt="user photo" />
+          <CardContent>
+            <Button fullWidth variant="contained" color="info" size='large'>
+              Edit
+            </Button>
+          </CardContent>
+          <CardActions disableSpacing></CardActions>
+          <Collapse timeout="auto" unmountOnExit></Collapse>
+        </Card>
       </Container>
-
-      <Popover
-        open={Boolean(open)}
-        anchorEl={open}
-        onClose={handleCloseMenu}
-        anchorOrigin={{ vertical: 'top', horizontal: 'left' }}
-        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-        PaperProps={{
-          sx: {
-            p: 1,
-            width: 140,
-            '& .MuiMenuItem-root': {
-              px: 1,
-              typography: 'body2',
-              borderRadius: 0.75,
-            },
-          },
-        }}
-      >
-        <MenuItem>
-          <Iconify icon={'eva:edit-fill'} sx={{ mr: 2 }} />
-          Edit
-        </MenuItem>
-
-        <MenuItem sx={{ color: 'error.main' }}>
-          <Iconify icon={'eva:trash-2-outline'} sx={{ mr: 2 }} />
-          Delete
-        </MenuItem>
-      </Popover>
     </>
   );
 }
