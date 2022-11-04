09ef5f62f30ba715e794dd09478e400172d3284a
ProDuck993 <produck.1993@gmail.com>
2022-11-04 16:11:42 +0700

    pull dev

--- src/pages/WalletPage.js
+++ src/pages/WalletPage.js
@@ -1,6 +1,17 @@
 import { Helmet } from 'react-helmet-async';
-import { useEffect, useState } from 'react';
-import { changeFlag } from '../features/flagSlice';
+import { forwardRef, useEffect, useState } from 'react';
+import { changeFlag } from "../features/flagSlice";
+import category from "../_mock/category";
+import Iconify from "../components/iconify";
+import wallet from '../_mock/wallet';
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import Swal from "sweetalert2";
+import dayjs from 'dayjs';
+import { DesktopDatePicker, LocalizationProvider } from '@mui/lab';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import * as React from 'react';
+
 import {
   Avatar,
   Box,
@@ -10,19 +21,23 @@ import {
   DialogContent,
   DialogContentText,
   DialogTitle,
+  FormControl,
   Grid,
+  InputLabel,
   List,
   ListItem,
   ListItemAvatar,
   ListItemText,
-  Paper, Stack,
-  TextField, Typography,
+  MenuItem,
+  Paper,
+  Select,
+  Slide,
+  TextField,
 } from '@mui/material';
-import axios from 'axios';
-import { useDispatch, useSelector } from 'react-redux';
-import Swal from 'sweetalert2';
-import Iconify from "../components/iconify";
 
+const Transition = React.forwardRef(function Transaction(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 function numberWithCommas(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
@@ -31,10 +46,43 @@ export default function WalletPage() {
   const [detail, setDetail] = useState(<h5>Choose wallet to see details</h5>);
   const [wallets, setWallets] = useState([]);
   const [walletEdit, setWalletEdit] = useState([]);
-  const [open, setOpen] = useState(false);
-  const [totalMoney, setTotalMoney] = useState(0);
 
-  const flag = useSelector((state) => state.flag);
+
+  // Create Wallet
+
+  const [openCreate, setOpenCreate] = React.useState(false);
+  const [open, setOpen] = React.useState(false);
+  const [wallet, setWallet] = useState()
+  const [value, setValue] = useState(dayjs());
+  const [openAddForm, setOpenAddForm] = useState(false);
+const idUser = JSON.parse(localStorage.getItem('user')).user_id
+
+  const handleClickOpenCreate = () => {
+    setOpenCreate(true);
+  };
+  const handleCloseCreate = () => {
+    setOpenCreate(false);
+  };
+
+  const handleChangeCreate = (e) => {
+    setWallet({
+      ...wallet,
+      [e.target.name]: e.target.value
+    });
+
+  };
+
+  const handleSubmitCreate = async () => {
+    setOpenAddForm(false);
+    
+
+    const result = await axios.post('http://localhost:3001/wallet/create')
+    console.log(result)
+  }
+
+
+  // done
+  const flag = useSelector(state => state.flag);
   const dispatch = useDispatch();
 
   const handleClickOpen = (id) => {
@@ -48,22 +96,14 @@ export default function WalletPage() {
   };
 
   const getAllWallet = async () => {
-    const userId = JSON.parse(localStorage.getItem('user'));
-    return await axios.get(` http://localhost:3001/wallet/get-all-wallet/${userId.user_id}`);
-  };
-
-  const toTalMoney = async () => {
-    const userId = JSON.parse(localStorage.getItem('user'));
-    return await axios.get(`http://localhost:3001/wallet/total/${userId.user_id}`)
-  };
+    const userId = JSON.parse(localStorage.getItem('user'))
+    return await axios.get(` http://localhost:3001/wallet/get-all-wallet/${userId.user_id}`)
+  }
 
   useEffect(() => {
-    getAllWallet()
-      .then((res) => setWallets(res.data.wallet))
-      .catch((error) => console.log(error.message));
-    toTalMoney().then(res => setTotalMoney(res.data.total))
-        .catch(error => console.log(error.message))
-  }, [flag]);
+    getAllWallet().then(res => setWallets(res.data.wallet)
+    ).catch(error => console.log(error.message))
+  }, [flag])
 
   const onChangeEdit = (e) => {
     if (e.target.name === 'amount') {
@@ -85,32 +125,31 @@ export default function WalletPage() {
     }).then(async (result) => {
       if (result.isConfirmed) {
         await axios.delete(`http://localhost:3001/wallet/delete/${id}`)
-            .then(res=> {
-              dispatch(changeFlag(1))
-              setDetail(<h5>Choose wallet to see details</h5>)
-            })
-            .catch(err => console.log(err))
+          .then(res => {
+            dispatch(changeFlag(1))
+            setDetail(<h5>Choose wallet to see details</h5>)
+          })
+          .catch(err => console.log(err))
         Swal.fire(
-            'Deleted!',
-            'Wallet has been deleted.',
-            'success'
+          'Deleted!',
+          'Wallet has been deleted.',
+          'success'
         )
       }
-    });
-  };
-
+    })
+  }
 
   const handleSaveEdit = async (id) => {
     await axios.put(`http://localhost:3001/wallet/update/${id}`, walletEdit)
-        .then(res=>{
-          Swal.fire({
-                icon: 'success',
-                title: 'Update Successfully!'
-          })
-          dispatch(changeFlag(1))
-          setDetail(<h5>Choose wallet to see details</h5>)
+      .then(res => {
+        Swal.fire({
+          icon: 'success',
+          title: 'Update Successfully!'
         })
-        .catch(err => console.log(err))
+        dispatch(changeFlag(1))
+        setDetail(<h5>Choose wallet to see details</h5>)
+      })
+      .catch(err => console.log(err))
     setOpen(false);
   };
 
@@ -140,22 +179,21 @@ export default function WalletPage() {
         <title> Wallet | Money Manager Master </title>
       </Helmet>
 
-      <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
-        <Typography variant="h3" gutterBottom>
-          Wallet
-        </Typography>
-        <Button variant="contained" startIcon={<Iconify icon="eva:plus-fill" />} >
-          New Wallet
-        </Button>
-      </Stack>
-
+      <h1>Wallet</h1>
       <Paper elevation={3} sx={{ padding: 2 }}>
         <Grid container spacing={2}>
           <Grid item xs={5} alignItems="center">
-            <h3>Total: {numberWithCommas(totalMoney)} VNĐ</h3>
+            <div>Total: 10.000.000</div>
           </Grid>
-          <Grid item xs={7}>
+          <Grid item xs={4}>
             <h3>Detail</h3>
+          </Grid>
+          <Grid item xs={3}>
+            <Box display="flex" justifyContent="flex-end">
+              <Button color="success" variant="contained" startIcon={<Iconify icon="eva:plus-fill" />} onClick={handleClickOpenCreate}>
+                Add new Wallet
+              </Button>
+            </Box>
           </Grid>
           <Grid item xs={5}>
             <List>
@@ -205,6 +243,62 @@ export default function WalletPage() {
         <DialogActions>
           <Button onClick={handleClose}>Cancel</Button>
           <Button onClick={() => handleSaveEdit(walletEdit._id)}>Save</Button>
+        </DialogActions>
+      </Dialog>
+
+      {/* Dialog create wallet/>*/}
+      <Button variant="outlined" onClick={handleClickOpenCreate}>
+        Slide in alert dialog
+      </Button>
+      <Dialog
+        TransitionComponent={Transition}
+        fullWidth={true}
+        maxWidth='md'
+        keepMounted
+        open={openCreate}
+        onClose={handleCloseCreate}>
+        <DialogTitle>{"Add Wallet"}</DialogTitle>
+
+        <DialogContent>
+          <DialogContentText>
+            Form dialogs allow users to fill out form fields within a dialog. For example, if your site prompts for potential subscribers to fill in their email address, they can fill out the email field and touch 'Submit'.
+          </DialogContentText>
+          <Grid container spacing={4}>
+            <Grid item xs={6}>
+              <TextField name="icon" onChange={handleChangeCreate} fullWidth={true} label="Icon" variant="outlined" />
+            </Grid>
+
+            <Grid item xs={6}>
+              <TextField name="name" onChange={handleChangeCreate} fullWidth={true} label="Name" variant="outlined" />
+            </Grid>
+
+            <Grid item xs={6}>
+              <TextField name="user_id" onChange={handleChangeCreate} fullWidth={true} variant="outlined" />{idUser} 
+            </Grid>
+            <Grid item xs={6}>
+              <TextField name="amount" onChange={handleChangeCreate} fullWidth={true} label="Amount" variant="outlined" type="number" />
+            </Grid>
+            <Grid item xs={6}>
+
+
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DesktopDatePicker
+                  fullWidth
+                  label="Date desktop"
+                  inputFormat="DD/MM/YYYY"
+                  value={value}
+                  name="date"
+                  onChange={handleChangeCreate}
+                  renderInput={(params) => <TextField {...params} />}
+                />
+              </LocalizationProvider>
+            </Grid>
+          </Grid>
+
+        </DialogContent>
+        <DialogActions>
+          <Button variant="outlined" onClick={handleCloseCreate}>Cancel</Button>
+          <Button variant="contained" color="success" onClick={handleSubmitCreate}>Save</Button>
         </DialogActions>
       </Dialog>
     </>
