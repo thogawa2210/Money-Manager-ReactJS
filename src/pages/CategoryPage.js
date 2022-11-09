3569880bb0c4d8357471c99f86be1e72bc518f7a
ProDuck993 <produck.1993@gmail.com>
2022-11-09 11:30:01 +0700

    crud categories

--- src/pages/CategoryPage.js
+++ src/pages/CategoryPage.js
@@ -1,6 +1,6 @@
 import { Helmet } from 'react-helmet-async';
 import { forwardRef, useEffect, useState } from 'react';
-import { Avatar, Box, Button, Card, CardContent, Container, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, FormControl, Grid, IconButton, InputLabel, MenuItem, Select, Slide, Stack, TableFooter, TablePagination, TextField, Typography } from '@mui/material';
+import { Avatar, Box, Button, Card, CardContent, Container, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, FormControl, Grid, IconButton, InputLabel, ListSubheader, MenuItem, Select, Slide, Stack, TableFooter, TablePagination, TextField, Typography } from '@mui/material';
 import Iconify from 'src/components/iconify';
 import Swal from 'sweetalert2';
 import axios from 'axios';
@@ -25,6 +25,18 @@ const Transition = forwardRef(function Transition(props, ref) {
 const TransitionEdit = forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
+
+const currencies = [
+  {
+    value: 'expense',
+    label: 'expense',
+  },
+  {
+    value: 'income',
+    label: 'income',
+  },
+];
+
 export default function ProductsPage() {
   const flag = useSelector((state) => state.flag);
   const dispatch = useDispatch();
@@ -38,6 +50,7 @@ export default function ProductsPage() {
     icon: ''
   })
   const [openAddForm, setOpenAddForm] = useState(false);
+
   const idUser = JSON.parse(localStorage.getItem('user')).user_id
   const handleClickOpenCreateCategory = () => {
     setOpenCreateCategory(true);
@@ -139,36 +152,85 @@ export default function ProductsPage() {
       }
     });
   };
-  // Update Category
-  const [categoryEdit, setCategoryEdit] = useState([])
 
-
-  const handleClickEditAddForm = (id) => {
-    const editCategory = categories.filter((categories) => categories._id === id);
-    setCategoryEdit(editCategory[0]);
-    setOpenEditCategory(true);
-  };
-
-  const [iconEdit, setIconEdit] = useState('');
+  //  Update Category
+  //  Xu li lay form du lieu
   const [openEditCategory, setOpenEditCategory] = useState(false);
+  const [iconEdit, setIconEdit] = useState('');
+  const [typeEdit, setTypeEdit] = useState('');
+  const [textEdit, setTextEdit] = useState('');
+  const [editForm, setEditForm] = useState({})
 
-  const handleCloseEdit = () => {
-    setCategoryEdit([])
-    setOpenEditCategory(false);
-  };
   const handleChangeIconEdit = (event) => {
     setIconEdit(event.target.value);
   };
-  const handleChangeEdit = (e) => {
-    setCategoryEdit({
-      ...categoryEdit,
-      [e.target.name]: e.target.value
-    });
+  const handleChangeTypeEdit = (event) => {
+    setTypeEdit(event.target.value);
   };
-  const handleSubmitEdit = async () => {
-
+  const handleChangeText = async (e) => {
+    setTextEdit({
+      ...textEdit,
+      [e.target.name]: e.target.value
+    })
   }
-  console.log(categoryEdit)
+  const data = {
+    icon: iconEdit,
+    name: textEdit.name,
+    type: typeEdit,
+    user_id: idUser
+  }
+  console.log(data)
+  // Xử lý hàm trả về thông tin update
+
+  const handleCloseEdit = () => {
+    setOpenEditCategory(false);
+  };
+  const handleClickOpenCategory = async (id) => {
+    await axios.get(`http://localhost:3001/category/get-category-id/${id}`)
+      .then(res => {
+        setEditForm(res.data.category)
+        setOpenEditCategory(true);
+      })
+  }
+
+  const handleSubmitCateEdit = async () => {
+    setOpenEditCategory(false);
+    if(data.name === "" || data.icon === "" || data.type === "") {
+      setOpenEditCategory(false);
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Please fill all the required fields',
+      });
+    } else {
+      Swal.fire({
+        title: 'Are you sure to edit?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, edit it!'
+      }).then(async (result) => {
+        if (result.isConfirmed) {
+          await axios.put(` http://localhost:3001/category/update-categody/${editForm._id}`, data)
+            .then(res => {
+              dispatch(changeFlag(1))
+            })
+            .catch(err => console.log(err))
+          Swal.fire(
+            'Edited!',
+            'Category has been edited.',
+            'success'
+          )
+        }
+      });
+    }
+  }
+
+
+
+
   return (
     <>
       <Grid container spacing={3}>
@@ -181,17 +243,16 @@ export default function ProductsPage() {
           </Stack>
         </Grid>
         <Grid item xs />
-        <Grid item xs={8} sx={{ padding: 0 }}>
+        <Grid item xs={9} sx={{ padding: 0 }}>
           <Stack>
             <Grid>
               <Grid>
                 <Card>
                   <CardContent sx={{ pb: 0 }}>
                     {/* Table */}
-                    <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+                    <Table sx={{ minWidth: 'auto' }} size="small" aria-label="a dense table">
                       <TableHead>
                         <TableRow>
-                          <TableCell>N.O</TableCell>
                           <TableCell align="center">Icon</TableCell>
                           <TableCell align="center">Name Category</TableCell>
                           <TableCell align="center">Type&nbsp;</TableCell>
@@ -199,25 +260,62 @@ export default function ProductsPage() {
                         </TableRow>
                       </TableHead>
                       <TableBody>
-                        {categories.map((item, index) => (
-                          <TableRow key={index}
+                  <ListSubheader  >Expense</ListSubheader>
+
+                        {categories.map((item,index)=> {
+                          if (item.type == "expense") 
+                          return (
+                            <TableRow key={index}
                             sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                           >
-                            <TableCell component="th" scope="row">{index + 1}</TableCell>
-                            <TableCell align="center"><Avatar src={item.icon}></Avatar></TableCell>
+                            <TableCell align="center"><Avatar src={item.icon} ></Avatar></TableCell>
                             <TableCell align="center"><strong>{item.name}</strong></TableCell>
-                            <TableCell align="right">{item.type}</TableCell>
-                            <TableCell align="right">
-                              <Button variant="outlined" color="success" onClick={() => handleClickEditAddForm(item._id)}>Edit</Button>
+                            <TableCell align="center">{item.type}</TableCell>
+                            <TableCell align="center">
+                              <Button variant="outlined" color="success" onClick={() => handleClickOpenCategory(item._id)}>Edit</Button>
                               <Button variant="outlined" color="error" onClick={() => handleDeleteCategory(item._id)}>Delete</Button>
                             </TableCell>
                           </TableRow>
-                        ))}
+                          )  
+                        })}
 
+                  <ListSubheader>InCome</ListSubheader>
+
+                         {categories.map((item,index)=> {
+                          if (item.type == "income") 
+                          return (
+                            <TableRow key={index}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                          >
+                            <TableCell align="center"><Avatar src={item.icon} ></Avatar></TableCell>
+                            <TableCell align="center"><strong>{item.name}</strong></TableCell>
+                            <TableCell align="center">{item.type}</TableCell>
+                            <TableCell align="center">
+                              <Button variant="outlined" color="success" onClick={() => handleClickOpenCategory(item._id)}>Edit</Button>
+                              <Button variant="outlined" color="error" onClick={() => handleDeleteCategory(item._id)}>Delete</Button>
+                            </TableCell>
+                          </TableRow>
+                          )  
+                        })}
+                        {/* {categories.map((item, index) => (
+                          <TableRow key={index}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                          >
+                            <TableCell align="center"><Avatar src={item.icon} ></Avatar></TableCell>
+                            <TableCell align="center"><strong>{item.name}</strong></TableCell>
+                            <TableCell align="center">{item.type}</TableCell>
+                            <TableCell align="center">
+                              <Button variant="outlined" color="success" onClick={() => handleClickOpenCategory(item._id)}>Edit</Button>
+                              <Button variant="outlined" color="error" onClick={() => handleDeleteCategory(item._id)}>Delete</Button>
+                            </TableCell>
+                          </TableRow>
+                        ))} */}
+                    
                       </TableBody>
                     </Table>
                     {/* Done Table */}
                   </CardContent>
+
                 </Card>
               </Grid>
             </Grid>
@@ -234,7 +332,7 @@ export default function ProductsPage() {
         keepMounted
         open={openCreateCategory}
         onClose={handleCloseCreate}>
-        <DialogTitle>{"Add Wallet"}</DialogTitle>
+        <DialogTitle>{"Add Category"}</DialogTitle>
         <DialogContentText>
         </DialogContentText>
         <DialogContent>
@@ -304,13 +402,13 @@ export default function ProductsPage() {
       </Dialog>
       {/* Update Category */}
       <Dialog
-        TransitionComponent={TransitionEdit}
+        TransitionComponent={Transition}
         fullWidth={true}
         maxWidth='md'
         keepMounted
         open={openEditCategory}
-        onClose={handleCloseEdit}>
-        <DialogTitle>{"Edit Wallet"}</DialogTitle>
+        onClose={handleCloseCreate}>
+        <DialogTitle>{"Edit Category"}</DialogTitle>
         <DialogContentText>
         </DialogContentText>
         <DialogContent>
@@ -325,11 +423,12 @@ export default function ProductsPage() {
                     id="demo-simple-select"
                     label="icon"
                     name="icon"
-                    value={categoryEdit.icon}
                     onChange={(event) => handleChangeIconEdit(event)}
                     sx={{ height: 55 }}
+                    placeholder={editForm.icon + ''}
                   >
-                    <MenuItem value={`/assets/icons/category/car.svg`}>
+
+                    <MenuItem value={`/assets/icons/category/car.svg`}  >
                       <Avatar src={`/assets/icons/category/car.svg`} sx={{ mr: 0 }} />
                     </MenuItem>
                     <MenuItem value={`/assets/icons/category/food.svg`}>
@@ -346,21 +445,20 @@ export default function ProductsPage() {
               </Box>
             </Grid>
             <Grid item xs={5} >
-              <TextField name="name" onChange={handleChangeEdit} fullWidth={true} label="Name Category" variant="outlined" value={categoryEdit.name}/>
+              <TextField name="name" onChange={(e) => handleChangeText(e)} fullWidth={true} variant="outlined" placeholder={editForm.name} />
             </Grid>
-            {/*  */}
             <Grid item xs={2}>
               <Box sx={{ minWidth: 120 }}>
                 <FormControl sx={{ width: 300 }}>
-                  <InputLabel id="demo-simple-select-label">Type</InputLabel>
-                  <Select
+
+                  {/* <Select
                     labelId="demo-simple-select-label"
                     id="demo-simple-select"
                     label="type"
                     name="type"
-                    value={categoryEdit.type}
-                    onChange={(event) => handleChangeType(event)}
+                    onChange={(event) => handleChangeTypeEdit(event)}
                     sx={{ height: 55 }}
+                    value={editForm.type + ''}
                   >
                     <MenuItem value={`expense`}>
                       Expense
@@ -368,16 +466,31 @@ export default function ProductsPage() {
                     <MenuItem value={`income`}>
                       Income
                     </MenuItem>
-                  </Select>
+                  </Select> */}
+
+                  <TextField
+                    id="outlined-select-currency"
+                    select
+                    label="Type"
+                    onChange={(event) => handleChangeTypeEdit(event)}
+                    placeholder={editForm.type + ''}
+                    helperText="Please select your currency"
+                  >
+                    {currencies.map((option) => (
+                      <MenuItem key={option.value} value={option.value}>
+                        {option.label}
+                      </MenuItem>
+                    ))}
+                  </TextField>
+
                 </FormControl>
               </Box>
             </Grid>
           </Grid>
-
         </DialogContent>
         <DialogActions>
           <Button variant="outlined" onClick={handleCloseEdit}>Cancel</Button>
-          <Button variant="contained" startIcon={<Iconify icon="uis:check" />} onClick={handleSubmitEdit}>Save</Button>
+          <Button variant="contained" startIcon={<Iconify icon="uis:check" />} onClick={handleSubmitCateEdit}>Save</Button>
         </DialogActions>
       </Dialog>
 
