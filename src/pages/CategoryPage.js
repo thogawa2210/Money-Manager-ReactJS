67cd79a0ce3a0ea15d4c91a959dc8fa8c2837a55
ProDuck993 <produck.1993@gmail.com>
2022-11-09 15:31:49 +0700

    categories

--- src/pages/CategoryPage.js
+++ src/pages/CategoryPage.js
@@ -1,6 +1,6 @@
 import { Helmet } from 'react-helmet-async';
 import { forwardRef, useEffect, useState } from 'react';
-import { Avatar, Box, Button, Card, CardContent, Container, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, FormControl, Grid, IconButton, InputLabel, ListSubheader, MenuItem, Select, Slide, Stack, TableFooter, TablePagination, TextField, Typography } from '@mui/material';
+import { Avatar, Box, Button, Card, CardContent, Container, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, FormControl, Grid, IconButton, InputLabel, ListSubheader, MenuItem, Select, Slide, Stack, Tab, TableFooter, TablePagination, Tabs, TextField, Typography } from '@mui/material';
 import Iconify from 'src/components/iconify';
 import Swal from 'sweetalert2';
 import axios from 'axios';
@@ -36,8 +36,47 @@ const currencies = [
     label: 'income',
   },
 ];
+// Tab Detail
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `vertical-tab-${index}`,
+    'aria-controls': `vertical-tabpanel-${index}`,
+  };
+}
 
 export default function ProductsPage() {
+  // Tab detail 
+  const [value, setValue] = useState(0);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  // Done
   const flag = useSelector((state) => state.flag);
   const dispatch = useDispatch();
   const [openCreateCategory, setOpenCreateCategory] = useState(false);
@@ -52,6 +91,7 @@ export default function ProductsPage() {
   const [openAddForm, setOpenAddForm] = useState(false);
 
   const idUser = JSON.parse(localStorage.getItem('user')).user_id
+
   const handleClickOpenCreateCategory = () => {
     setOpenCreateCategory(true);
   };
@@ -79,8 +119,8 @@ export default function ProductsPage() {
         icon: 'error',
         title: 'Oops...',
         text: 'Please fill all the required fields',
-        showConfirmButton : false,
-        timer : 1500
+        showConfirmButton: false,
+        timer: 1500
       });
     } else {
       const result = await axios.post('http://localhost:3001/category/add-category', data)
@@ -88,9 +128,9 @@ export default function ProductsPage() {
         Swal.fire({
           icon: 'success',
           title: 'Create Category Successfully!',
-          showConfirmButton : false,
-          timer : 1500
-         
+          showConfirmButton: false,
+          timer: 1500
+
         }).then(
           setOpenCreateCategory(false),
           dispatch(changeFlag(1)),
@@ -104,8 +144,8 @@ export default function ProductsPage() {
         Swal.fire({
           icon: 'warning',
           title: 'Your name of category already exist',
-          showConfirmButton : false,
-          timer : 1500
+          showConfirmButton: false,
+          timer: 1500
         })
         setOpenCreateCategory(false)
         setCategory({
@@ -151,13 +191,13 @@ export default function ProductsPage() {
           })
           .catch(err => console.log(err))
         Swal.fire({
-          icon : 'success',
-          text : 'Deleted!',
-          title : 'Category has been deleted.',
-          showConfirmButton : false,
-          timer : 1500
+          icon: 'success',
+          text: 'Deleted!',
+          title: 'Category has been deleted.',
+          showConfirmButton: false,
+          timer: 1500
         })
-     
+
       }
     });
   };
@@ -204,14 +244,14 @@ export default function ProductsPage() {
 
   const handleSubmitCateEdit = async () => {
     setOpenEditCategory(false);
-    if(data.name === "" || data.icon === "" || data.type === "") {
+    if (data.name === "" || data.icon === "" || data.type === "") {
       setOpenEditCategory(false);
       Swal.fire({
         icon: 'error',
         title: 'Oops...',
         text: 'Please fill all the required fields',
-        showConfirmButton : false,
-        timer : 1500
+        showConfirmButton: false,
+        timer: 1500
       });
     } else {
       Swal.fire({
@@ -230,16 +270,17 @@ export default function ProductsPage() {
             })
             .catch(err => console.log(err))
           Swal.fire({
-            icon : 'success',
-            title : 'Edited!',
-            text : 'Category has been edited.',
-            showConfirmButton : false,
-            timer : 1500
-        })
+            icon: 'success',
+            title: 'Edited!',
+            text: 'Category has been edited.',
+            showConfirmButton: false,
+            timer: 1500
+          })
         }
       });
     }
   }
+
 
 
 
@@ -263,7 +304,23 @@ export default function ProductsPage() {
                 <Card>
                   <CardContent sx={{ pb: 0 }}>
                     {/* Table */}
-                    <Table sx={{ minWidth: 'auto' }} size="small" aria-label="a dense table">
+                    <Box
+                      sx={{ flexGrow: 10, bgcolor: 'background.paper', display: 'flex', height: 400 }}
+                    >
+                      <Tabs
+                        orientation="vertical"
+                        variant="scrollable"
+                        value={value}
+                        onChange={handleChange}
+                        aria-label="Vertical tabs example"
+                        sx={{ borderRight: 1, borderColor: 'divider' }}
+                      >
+                        <Tab label="InCome" {...a11yProps(0)} />
+                        <Tab label="Expense" {...a11yProps(1)} />
+
+                      </Tabs>
+                      <TabPanel value={value} index={0}>
+                      <Table sx={{ minWidth: 200 }} size="small" aria-label="a dense table">
                       <TableHead>
                         <TableRow>
                           <TableCell align="center">Icon</TableCell>
@@ -273,47 +330,117 @@ export default function ProductsPage() {
                         </TableRow>
                       </TableHead>
                       <TableBody>
-                  <ListSubheader  >Expense</ListSubheader>
-
-                        {categories.map((item,index)=> {
-                          if (item.type == "expense") 
-                          return (
-                            <TableRow key={index}
-                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                          >
-                            <TableCell align="center"><Avatar src={item.icon} ></Avatar></TableCell>
-                            <TableCell align="center"><strong>{item.name}</strong></TableCell>
-                            <TableCell align="center">{item.type}</TableCell>
-                            <TableCell align="center">
-                              <Button variant="outlined" color="success" onClick={() => handleClickOpenCategory(item._id)}>Edit</Button>
-                              <Button variant="outlined" color="error" onClick={() => handleDeleteCategory(item._id)}>Delete</Button>
-                            </TableCell>
-                          </TableRow>
-                          )  
+                
+                        {categories.map((item, index) => {
+                          if (item.type == "income")
+                            return (
+                              <TableRow key={index}
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                              >
+                                <TableCell align="center"><Avatar src={item.icon} ></Avatar></TableCell>
+                                <TableCell align="center"><strong>{item.name}</strong></TableCell>
+                                <TableCell align="center">{item.type}</TableCell>
+                                <TableCell align="center">
+                                  <Button variant="outlined" color="success" onClick={() => handleClickOpenCategory(item._id)}>Edit</Button>
+                                  <Button variant="outlined" color="error" onClick={() => handleDeleteCategory(item._id)}>Delete</Button>
+                                </TableCell>
+                              </TableRow>
+                            )
                         })}
 
-                  <ListSubheader>InCome</ListSubheader>
 
-                         {categories.map((item,index)=> {
-                          if (item.type == "income") 
-                          return (
-                            <TableRow key={index}
-                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                          >
-                            <TableCell align="center"><Avatar src={item.icon} ></Avatar></TableCell>
-                            <TableCell align="center"><strong>{item.name}</strong></TableCell>
-                            <TableCell align="center">{item.type}</TableCell>
-                            <TableCell align="center">
-                              <Button variant="outlined" color="success" onClick={() => handleClickOpenCategory(item._id)}>Edit</Button>
-                              <Button variant="outlined" color="error" onClick={() => handleDeleteCategory(item._id)}>Delete</Button>
-                            </TableCell>
-                          </TableRow>
-                          )  
-                        })}
-         
-                    
                       </TableBody>
                     </Table>
+                      </TabPanel>
+                      <TabPanel value={value} index={1}>
+                      <Table sx={{ minWidth: 'auto' }} size="small" aria-label="a dense table">
+                      <TableHead>
+                        <TableRow>
+                          <TableCell align="center">Icon</TableCell>
+                          <TableCell align="center">Name Category</TableCell>
+                          <TableCell align="center">Type&nbsp;</TableCell>
+                          <TableCell align="center">Action</TableCell>
+                        </TableRow>
+                      </TableHead>
+                      <TableBody>
+                        
+
+                        {categories.map((item, index) => {
+                          if (item.type == "expense")
+                            return (
+                              <TableRow key={index}
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                              >
+                                <TableCell align="center"><Avatar src={item.icon} ></Avatar></TableCell>
+                                <TableCell align="center"><strong>{item.name}</strong></TableCell>
+                                <TableCell align="center">{item.type}</TableCell>
+                                <TableCell align="center">
+                                  <Button variant="outlined" color="success" onClick={() => handleClickOpenCategory(item._id)}>Edit</Button>
+                                  <Button variant="outlined" color="error" onClick={() => handleDeleteCategory(item._id)}>Delete</Button>
+                                </TableCell>
+                              </TableRow>
+                            )
+                        })}
+
+          
+
+
+                      </TableBody>
+                    </Table>
+                      </TabPanel>
+                    </Box>
+
+                    {/* <Table sx={{ minWidth: 'auto' }} size="small" aria-label="a dense table">
+                      <TableHead>
+                        <TableRow>
+                          <TableCell align="center">Icon</TableCell>
+                          <TableCell align="center">Name Category</TableCell>
+                          <TableCell align="center">Type&nbsp;</TableCell>
+                          <TableCell align="center">Action</TableCell>
+                        </TableRow>
+                      </TableHead>
+                      <TableBody>
+                        <ListSubheader  >Expense</ListSubheader>
+
+                        {categories.map((item, index) => {
+                          if (item.type == "expense")
+                            return (
+                              <TableRow key={index}
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                              >
+                                <TableCell align="center"><Avatar src={item.icon} ></Avatar></TableCell>
+                                <TableCell align="center"><strong>{item.name}</strong></TableCell>
+                                <TableCell align="center">{item.type}</TableCell>
+                                <TableCell align="center">
+                                  <Button variant="outlined" color="success" onClick={() => handleClickOpenCategory(item._id)}>Edit</Button>
+                                  <Button variant="outlined" color="error" onClick={() => handleDeleteCategory(item._id)}>Delete</Button>
+                                </TableCell>
+                              </TableRow>
+                            )
+                        })}
+
+                        <ListSubheader>InCome</ListSubheader>
+
+                        {categories.map((item, index) => {
+                          if (item.type == "income")
+                            return (
+                              <TableRow key={index}
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                              >
+                                <TableCell align="center"><Avatar src={item.icon} ></Avatar></TableCell>
+                                <TableCell align="center"><strong>{item.name}</strong></TableCell>
+                                <TableCell align="center">{item.type}</TableCell>
+                                <TableCell align="center">
+                                  <Button variant="outlined" color="success" onClick={() => handleClickOpenCategory(item._id)}>Edit</Button>
+                                  <Button variant="outlined" color="error" onClick={() => handleDeleteCategory(item._id)}>Delete</Button>
+                                </TableCell>
+                              </TableRow>
+                            )
+                        })}
+
+
+                      </TableBody>
+                    </Table> */}
                     {/* Done Table */}
                   </CardContent>
 
@@ -403,7 +530,7 @@ export default function ProductsPage() {
       </Dialog>
       {/* Update Category */}
       <Dialog
-        TransitionComponent={Transition}
+        TransitionComponent={TransitionEdit}
         fullWidth={true}
         maxWidth='md'
         keepMounted
@@ -476,7 +603,7 @@ export default function ProductsPage() {
                     onChange={(event) => handleChangeTypeEdit(event)}
                     placeholder={editForm.type + ''}
                     helperText="Please select your currency"
-         
+
                   >
                     {currencies.map((option) => (
                       <MenuItem key={option.value} value={option.value}>
@@ -492,9 +619,11 @@ export default function ProductsPage() {
         </DialogContent>
         <DialogActions>
           <Button variant="outlined" color="error" onClick={handleCloseEdit}>Cancel</Button>
-          <Button variant="outlined"  color="success" onClick={handleSubmitCateEdit}>Submit</Button>
+          <Button variant="outlined" color="success" onClick={handleSubmitCateEdit}>Submit</Button>
         </DialogActions>
       </Dialog>
+
+      {/* Table cuon */}
 
     </>
   );
